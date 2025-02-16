d30bb238334273b262e82ab5ebd2e0464bfb72e5
Jed Henry <156385037+JedHenry50@users.noreply.github.com>
2025-02-16 21:28:10 +0000

    Rough draft for background.h

--- PA2_Shell/background.h
+++ PA2_Shell/background.h
@@ -1,1 +1,19 @@
 //This is the header file for background.c
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+#include <sys/types.h>
+
+// Struct to keep track of background processes (optional)
+typedef struct BackgroundProcess {
+    pid_t pid;                 // Process ID
+    struct BackgroundProcess *next; // Pointer to next process in a linked list
+} BackgroundProcess;
+
+// Function declarations
+void add_background_process(pid_t pid);
+void check_background_processes();
+void cleanup_background_process(pid_t pid);
+
+#endif // BACKGROUND_H
+
